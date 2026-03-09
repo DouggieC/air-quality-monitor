6d4d09c98578ee9a76e53a48d4a48951b5c98c6b
Doug Cooper <doug_cooper2000@yahoo.co.uk>
2026-03-09 21:44:02 +0100

    Rename logs to log

--- scripts/run_aq_monitor.sh
+++ scripts/run_aq_monitor.sh
@@ -4,7 +4,7 @@
 
 APP_DIR="/home/doug/projects/air-quality-monitor"
 PYTHON="$APP_DIR/.venv/bin/python"
-LOG_DIR="$APP_DIR/logs"
+LOG_DIR="$APP_DIR/log"
 LOCKFILE="/tmp/air_quality_monitor.lock"
 
 cd $APP_DIR || exit 1
