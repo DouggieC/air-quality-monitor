cab9415c42450d82be36156077c588d00111774d
DouggieC <doug_cooper2000@yahoo.co.uk>
2026-03-11 16:26:56 +0100

    Improve error handling in Config class

--- src/air_quality_monitor/config.py
+++ src/air_quality_monitor/config.py
@@ -7,7 +7,7 @@ from .models import City
 #load_dotenv(Path(__file__).parent.parent.parent / '.env')  # Load environment variables from .env file
 
 class Config:
-    
+        
     load_dotenv() # Load environment variables from .env file
 
     IQAIR_API_KEY = os.getenv('IQAIR_API_KEY')
@@ -18,10 +18,12 @@ class Config:
     LOG_DIR = Path(BASE_DIR / 'log')
     LOG_LEVEL = os.getenv('LOG_LEVEL')
 
-    #print(f'Config loaded: IQAIR_API_KEY={IQAIR_API_KEY}, IQAIR_BASE_URL={IQAIR_BASE_URL}, BASE_DIR={BASE_DIR}, DATA_DIR={DATA_DIR}')
-
     @classmethod
     def load_cities(cls) -> list[City]:
-        with open(cls.CITY_LIST) as f:
-            data = json.load(f)
+        try:
+            with open(cls.CITY_LIST) as f:
+                data = json.load(f)
+        except Exception as e:
+            raise
+        
         return [City(**item) for item in data]
