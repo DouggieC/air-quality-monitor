039459978afde394054f3440e72b76f82e910763
DouggieC <doug_cooper2000@yahoo.co.uk>
2026-03-18 15:42:53 +0100

    Implement basic DB connection configuration

--- src/air_quality_monitor/config.py
+++ src/air_quality_monitor/config.py
@@ -8,29 +8,46 @@ class Config:
         
     load_dotenv() # Load environment variables from .env file
 
+    # IQAir credentials & URLs
     IQAIR_API_KEY = os.getenv('IQAIR_API_KEY')
     IQAIR_BASE_URL = os.getenv('IQAIR_BASE_URL')
 
+    # OWM credentials & URLs
     OWM_API_KEY = os.getenv('OWM_API_KEY')
     OWM_ONECALL_BASE_URL = os.getenv('OWM_ONECALL_BASE_URL')
     OWM_GEO_BASE_URL = os.getenv('OWM_GEO_BASE_URL')
 
+    # Installation-specific directory structure
     BASE_DIR = Path(os.getenv('BASE_DIR'))
-    #DATA_DIR = Path(BASE_DIR / 'data')
     DATA_DIR = BASE_DIR / 'data'
-    #CITY_LIST = Path(os.getenv('CITY_LIST'))
-    #CONFIG_DIR = Path(BASE_DIR / 'config')
     CONFIG_DIR = BASE_DIR / 'config'
-    #LOG_DIR = Path(BASE_DIR / 'log')
     LOG_DIR = BASE_DIR / 'log'
-    LOG_LEVEL = os.getenv('LOG_LEVEL')
+    
+    # Required log level. Defaults to INFO
+    LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
 
+    # Is this a production system?
     IS_PRODUCTION = os.getenv('IS_PRODUCTION', 'false').lower() == 'true'
 
+    # File with list of cities depends on production flag.
+    # If not prod, use a shorter list of cities to save API calls
     city_file = 'cities.json' if IS_PRODUCTION else 'cities_dev.json'
     CITY_LIST = CONFIG_DIR / city_file
 
+    # Database details
+    # Are we using a database? Default 'false'
+    USE_DB = os.getenv('USE_DB', 'false').lower() == 'true'
+    if USE_DB:
+        DB_TYPE = os.getenv('DB_TYPE')
+        DB_USER = os.getenv('DB_USER')
+        DB_PASS = os.getenv('DB_PASS')
+        DB_HOST = os.getenv('DB_HOST')
+        DB_PORT = os.getenv('DB_PORT')
+        DB_NAME = os.getenv('DB_NAME')
+        
+
     @classmethod
+    # Load the list of required cities
     def load_cities(cls) -> list[City]:
         try:
             with open(cls.CITY_LIST) as f:
@@ -39,3 +56,13 @@ class Config:
             raise
         
         return [City(**item) for item in data]
+    
+    @classmethod
+    # Construct a DB connection URL
+    def get_db_url(cls) -> str:
+        if cls.DB_TYPE == 'sqlite':
+            return f'sqlite:///{cls.DATA_DIR}/{cls.DB_NAME}.db'
+        else:
+            return f'{cls.DB_TYPE}://{cls.DB_USER}:{cls.DB_PASS}@{cls.DB_HOST}:{cls.DB_PORT}/{cls.DB_NAME}'
+    
+    
