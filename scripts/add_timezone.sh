a87d862dbb7811eeca0af5b4889c775a66a8ff15
Doug Cooper <doug_cooper2000@yahoo.co.uk>
2026-04-12 10:13:43 +0200

    Fix timezone add script

--- scripts/add_timezone.sh
+++ scripts/add_timezone.sh
@@ -4,33 +4,36 @@ for file in we_history.csv; do
     output_file="${file%.csv}_fixed.csv"
     awk -F',' 'BEGIN { OFS="," }
 
-    # Header row: insert "timezone" after column 3 (country)
+    # Header row: always rebuild with timezone inserted if missing
     NR == 1 {
-        # Print cols 1-3, inject timezone header, then the rest
-        for (i = 1; i <= 3; i++)      printf "%s%s", $i, OFS
-        printf "\"timezone\""
-        for (i = 4; i <= NF; i++)     printf "%s%s", OFS, $i
-        printf "\n"
+        if (NF == 24) {
+            for (i = 1; i <= 3; i++)  printf "%s%s", $i, OFS
+            printf "\"timezone\""
+            for (i = 4; i <= NF; i++) printf "%s%s", OFS, $i
+            printf "\n"
+        } else {
+            print  # already has timezone header
+        }
         next
     }
 
-    # Corrupt rows: insert the correct timezone value after column 3
-    NR <= 4529 {
-        city = $1
-        gsub(/"/, "", city)
-        gsub(/\r/, "", city)
+    # Data rows: insert timezone if missing (24 cols), else pass through
+    {
+        if (NF == 24) {
+            city = $1
+            gsub(/"/, "", city)
+            gsub(/\r/, "", city)
 
-        tz = (city == "Bern") ? "\"Europe/Zurich\"" : "\"Europe/" city "\""
+            tz = (city == "Bern") ? "\"Europe/Zurich\"" : "\"Europe/" city "\""
 
-        for (i = 1; i <= 3; i++)      printf "%s%s", $i, OFS
-        printf "%s", tz
-        for (i = 4; i <= NF; i++)     printf "%s%s", OFS, $i
-        printf "\n"
-        next
+            for (i = 1; i <= 3; i++)  printf "%s%s", $i, OFS
+            printf "%s", tz
+            for (i = 4; i <= NF; i++) printf "%s%s", OFS, $i
+            printf "\n"
+        } else {
+            print  # already 25 cols, leave untouched
+        }
     }
-
-    # Rows 4530+: already correct, pass through untouched
-    { print }
 
     ' "$file" > "$output_file"
 done
